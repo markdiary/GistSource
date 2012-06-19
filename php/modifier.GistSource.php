be4e486cecbf73ddf2043d1258a98e5faf972920
maRk <mark45u.u4gm2006@gmail.com>
2012-06-19 11:29:00 +0900

    amend typo

--- php/modifier.GistSource.php
+++ php/modifier.GistSource.php
@@ -7,8 +7,8 @@ function smarty_modifier_GistSource( $str, $args) {
 		$ctx =& $mt->context();
 		$blog_id = 'blog:' . $ctx->stash('blog_id');
 		$conf = empty($conf)? $mt->db()->fetch_plugin_data ( 'GistSource', 'configuration:' . $blog_id ) : $conf;
-		$doGitSource = $conf['doGitSource'];
-		if ( $doGitSource !== 1 ){ return $str; };
+		$doGistSource = $conf['doGistSource'];
+		if ( $doGsitSource !== 1 ){ return $str; };
 		
 	$str = preg_replace_callback('/(?:\[)gist:(?:\s+)?([0-9]{1,9})(?:\])/'
 		, gist
