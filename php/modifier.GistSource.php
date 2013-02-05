8d3f9c189c4462aa7e5851397ef1669485403aee
maRk <mark45u.u4gm2006@gmail.com>
2013-02-05 09:27:07 +0900

    Fixed problem of Regular expression.

--- php/modifier.GistSource.php
+++ php/modifier.GistSource.php
@@ -3,23 +3,19 @@ function smarty_modifier_GistSource( $str, $args) {
 	if ($args != 1) {
 		return $str;
 	}
-		$mt = empty($mt)? (MT::get_instance()) : $mt;
-		$ctx =& $mt->context();
-		$blog_id = 'blog:' . $ctx->stash('blog_id');
-		$conf = empty($conf)? $mt->db()->fetch_plugin_data ( 'GistSource', 'configuration:' . $blog_id ) : $conf;
-		$doGistSource = $conf['doGistSource'];
-		if ( $doGsitSource !== 1 ){ return $str; };
-		
-	$str = preg_replace_callback('/(?:\[)gist:(?:\s+)?([0-9]{1,9})(?:\])/'
-		, gist
-		, $str);
+	$str = preg_replace_callback('/(?:\[)gist:(?:\s.+)?([0-9]{1,9})(?:\])/', "gist" , $str, 5);
 	return $str;
 }
 function gist($matches){
-		global $conf;
-		$add_type_attr = $conf['addTypeAttribute'];
-		$type  = $add_type_attr ==='1' ? ' type="text/javascript"' : '';
-		$line = '\n'.'<script src="https://gist.github.com/' .$matches[1]. '\.js"' .$type. '></script>'.'\n';
-	}
-return $line;
+    $mt = empty($mt)? (MT::get_instance()) : $mt;
+    $ctx =& $mt->context();
+    $blog_id = 'blog:' . $ctx->stash('blog_id');
+    $conf = $mt->db()->fetch_plugin_data ( 'GistSource', 'configuration:' . $blog_id );
+    $doGistSource = $conf['doGistSource'];
+    if ( $doGsitSource == 0 ){ return $matches[0]; }
+    $add_type_attr = $conf['addTypeAttribute'];
+    $type  = $add_type_attr =='1' ? ' type="text/javascript"' : '';
+    $line = '<script src="https://gist.github.com/' .$matches[1]. '.js"' .$type. '></script>';
+    return $line;
 }
+?>
